0ce291880279a23ae3632c725ff1adbd0692fec6
dinapirana <122113432+dinapirana@users.noreply.github.com>
2023-05-14 17:52:07 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -20,7 +20,17 @@ def decrypt_file(input_file_path, output_file_path, key):
     block_size = DES3.block_size
     with open(input_file_path, 'rb') as input_file, open(output_file_path, 'wb') as output_file:
 
+iv = input_file.read(block_size)
+        cipher = DES3.new(key, DES3.MODE_CBC, iv)
+        while True:
+            chunk = input_file.read(1024 * block_size)
+            if len(chunk) == 0:
+                break
+            output_file.write(cipher.decrypt(chunk))
 
+if name == 'main':
+    key = b'\x01\x23\x45\x67\x89\xab\xcd\xef\x87\x65\x43\x21\xfe\xdc\xba\x98'
+        
 input_file_path = r'C:\Users\Online\Desktop\Siguri1\vlera.txt'
     encrypted_file_path = r'C:\Users\Online\Desktop\Siguri1\encrypted_file.bin'
     decrypted_file_path = r'C:\Users\Online\Desktop\Siguri1\decrypted_file.txt'
