65cf53c47e5ec81fd73ce68078d42f29eb5ced63
Vlera Islami <117523183+VleraIslami@users.noreply.github.com>
2023-05-14 12:10:18 +0200

    Added the library

--- Code.py
+++ Code.py
@@ -1,1 +1,10 @@
+from Crypto.Cipher import DES3
+import os
 
+
+def encrypt_file(input_file_path, output_file_path, key):
+    block_size = DES3.block_size
+    iv = os.urandom(block_size)
+    cipher = DES3.new(key, DES3.MODE_CBC, iv)
+    with open(input_file_path, 'rb') as input_file, open(output_file_path, 'wb') as output_file:
+        output_file.write(iv)
