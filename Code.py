38f302c78064130b36bcb839d16fef10a07ce2ef
Vlera Islami <117523183+VleraIslami@users.noreply.github.com>
2023-05-14 18:26:14 +0200

    changed space

--- Code.py
+++ Code.py
@@ -9,12 +9,12 @@ def encrypt_file(input_file_path, output_file_path, key1, key2):
     cipher2 = DES3.new(key2, DES3.MODE_CBC, iv=iv)
     with open(input_file_path, 'rb') as input_file, open(output_file_path, 'wb') as output_file:
         output_file.write(iv)
-  while True:
-            chunk = input_file.read(1024 * block_size)
-            if len(chunk) == 0:
-                break
-            elif len(chunk) % block_size != 0:
-                chunk += b' ' * (block_size - len(chunk) % block_size)
-            ciphered_chunk = cipher.encrypt(chunk)
-            ciphered_chunk2 = cipher2.encrypt(ciphered_chunk)
-            output_file.write(ciphered_chunk2)      
+    while True:
+        chunk = input_file.read(1024 * block_size)
+        if len(chunk) == 0:
+            break
+        elif len(chunk) % block_size != 0:
+            chunk += b' ' * (block_size - len(chunk) % block_size)
+        ciphered_chunk = cipher.encrypt(chunk)
+        ciphered_chunk2 = cipher2.encrypt(ciphered_chunk)
+        output_file.write(ciphered_chunk2)
